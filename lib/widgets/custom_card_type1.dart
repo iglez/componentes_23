33dafd2a3471fc2b3acae2295aefaabde03ed765
Ivan Gonzalez <ivan@iconos.mx>
2023-02-13 22:16:42 -0800

    agregado dos botones a card

--- lib/widgets/custom_card_type1.dart
+++ lib/widgets/custom_card_type1.dart
@@ -9,11 +9,24 @@ class CustomCardType1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Column(
-        children: const [
-          ListTile(
+        children: [
+          const ListTile(
             leading: Icon(Icons.document_scanner),
             title: Text('Soy un titulo'),
             subtitle: Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {}, 
+                child: const Text('Cancel')
+              ),
+              TextButton(
+                onPressed: () {}, 
+                child: const Text('Ok')
+              ),
+            ],
           )
         ],
       ),
