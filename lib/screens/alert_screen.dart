6ce0a669eaf545c7ee05a1902e1fb8aab92158b0
Ivan Gonzalez <ivan@iconos.mx>
2023-02-20 17:44:02 -0800

    creando un metodo para alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,19 +3,24 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
+  void displayDialog() {
+    print('hola mundo');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: ElevatedButton(
-        // onPressed: null, 
-        onPressed: () {}, 
-        child: const Text('Mostrar Alerta')
-        )
-      ),
+      body: Center(
+          child: ElevatedButton(
+              // onPressed: null,
+              onPressed: displayDialog,
+              child: const Text('Mostrar Alerta'))),
       floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.red,
           child: const Icon(Icons.close),
-          onPressed: () {}),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
     );
   }
 }
