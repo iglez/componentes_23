dfb768de07b51eb651f56d3e1d33592957eb62f9
Ivan Gonzalez <ivan@iconos.mx>
2023-02-20 17:26:04 -0800

    Enviando el parametro de la imagen al widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -17,13 +17,21 @@ class CardScreen extends StatelessWidget {
           // Extract Widget
           CustomCardType1(),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+            imageUrl: 'https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?w=2000',
+          ),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+            imageUrl: 'https://img.freepik.com/premium-vector/mountain-green-field-alpine-landscape-nature-with-wooden-houses_194708-1779.jpg?w=2000',
+          ),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Landscape.svg/2560px-Landscape.svg.png',
+          ),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+            imageUrl: 'https://static.vecteezy.com/system/resources/previews/002/312/417/original/desert-hill-road-landscape-background-free-vector.jpg',
+          ),
         ],
       ),
     );
--- lib/widgets/custom_card_type2.dart
+++ lib/widgets/custom_card_type2.dart
@@ -1,35 +1,38 @@
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  
+  final String imageUrl;
+  
+  const CustomCardType2({
+    super.key, 
+    required this.imageUrl
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
       clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(18)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
       elevation: 30,
       // shadowColor: Colors.amber,
       child: Column(
         children: [
-          const FadeInImage(
+          FadeInImage(
             placeholder: AssetImage('assets/jar-loading.gif'),
-            image: NetworkImage('https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?w=2000'),
+            image: NetworkImage( imageUrl ),
             width: double.infinity,
             height: 260,
             fit: BoxFit.cover,
           ),
           Container(
-            alignment: AlignmentDirectional.centerEnd,
-            padding: const EdgeInsets.only(
-              right: 20,
-              bottom: 10,
-              top: 10,
-            ),
-            child: Text('Un hermoso paisaje')
-          )
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(
+                right: 20,
+                bottom: 10,
+                top: 10,
+              ),
+              child: Text('Un hermoso paisaje'))
         ],
       ),
     );
