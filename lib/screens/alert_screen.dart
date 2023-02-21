eea79b02e55b171d689b73a3318ad70b8bfef28d
Ivan Gonzalez <ivan@iconos.mx>
2023-02-20 17:48:50 -0800

    desplegando un alert dialog

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
-  void displayDialog() {
-    print('hola mundo');
+  void displayDialog(BuildContext context) {
+    // print('hola mundo');
+    showDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context) {
+          // return Text('Alerta?');
+          return AlertDialog(
+            elevation: 5,
+            title: const Text('Titulo Alerta'),
+            content: Column(
+              children: const [
+                Text('Este es el contenido de la alerta')
+              ]
+            ),
+          );
+        });
   }
 
   @override
@@ -13,7 +28,7 @@ class AlertScreen extends StatelessWidget {
       body: Center(
           child: ElevatedButton(
               // onPressed: null,
-              onPressed: displayDialog,
+              onPressed: () => displayDialog(context),
               child: const Text('Mostrar Alerta'))),
       floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.red,
