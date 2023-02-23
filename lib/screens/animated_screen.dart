b5ea6dbf653c291e5adb98b0901a79221570e255
Ivan Gonzalez <ivan@iconos.mx>
2023-02-22 16:31:12 -0800

    conviertiendo a un statefull

--- lib/screens/animated_screen.dart
+++ lib/screens/animated_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   const AnimatedScreen({super.key});
 
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
