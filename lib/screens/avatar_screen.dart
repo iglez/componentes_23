f136999d849c04b52e6eb1b1966db547c9f53c98
Ivan Gonzalez <ivan@iconos.mx>
2023-02-22 16:17:28 -0800

    Agregando un circle Avatar

--- lib/screens/avatar_screen.dart
+++ lib/screens/avatar_screen.dart
@@ -7,7 +7,12 @@ class AvatarScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Avatars'),
+        title: const Text('Ivan Gonzalez'),
+        actions: const [
+          CircleAvatar(
+            child: Text('IG'),
+          )
+        ],
       ),
       body: const Center(
         child: Text('Avatar Screen')
