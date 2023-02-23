45481b12a253a931ff784350ee73bcc7f7e76b66
Ivan Gonzalez <ivan@iconos.mx>
2023-02-22 16:34:58 -0800

    Agregando propiedades al container

--- lib/screens/animated_screen.dart
+++ lib/screens/animated_screen.dart
@@ -14,7 +14,17 @@ class _AnimatedScreenState extends State<AnimatedScreen> {
       appBar: AppBar(
         title: const Text('Aminated Container')
       ),
-      body: Container(),
+      body: Center(
+        child: Container(
+          // color: Colors.red,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(20)
+          ),
+        ),
+      ),
     );
   }
 }
