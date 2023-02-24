ec2fa9eb4aea2d1786b69592bfe9874b0aebff6f
Ivan Gonzalez <ivan@iconos.mx>
2023-02-23 16:56:46 -0800

    agregando app bar

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -5,7 +5,10 @@ class InputScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inputs y Forms'),
+      ),
       body: Center(child: Text('Text Input')),
     );
   }
