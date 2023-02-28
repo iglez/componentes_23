8aef5fada92db86b401dfa6c8d4c99576e84423f
Ivan Gonzalez <ivan@iconos.mx>
2023-02-27 22:38:35 -0800

    creando form key para formulario

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -6,6 +6,16 @@ class InputScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'Ivan',
+      'last_name': 'Gonzalez',
+      'email': 'ivan.gonzalez@cetys.mx',
+      'password': '123456',
+      'role': 'Admin'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs y Forms'),
@@ -17,35 +27,41 @@ class InputScreen extends StatelessWidget {
             vertical: 10,
           ),
           child: Form(
+            key: myFormKey,
             child: Column(
               children: [
-          
                 const CustomInputField(
                   labelText: 'Nombre',
                   hintText: 'Nombre del usuario',
                 ),
-                const SizedBox( height:30 ),
-                
-                const CustomInputField(labelText: 'Apellido', hintText: 'Apellido del usuario',),
-                const SizedBox( height:30 ),
-                
-                const CustomInputField(labelText: 'Correo', hintText: 'Correo del usuario', keyboardType: TextInputType.emailAddress,),
-                const SizedBox( height:30 ),
-                
-                const CustomInputField(labelText: 'Contraseña', 
-                  hintText: 'Contraseña', 
+                const SizedBox(height: 30),
+                const CustomInputField(
+                  labelText: 'Apellido',
+                  hintText: 'Apellido del usuario',
+                ),
+                const SizedBox(height: 30),
+                const CustomInputField(
+                  labelText: 'Correo',
+                  hintText: 'Correo del usuario',
                   keyboardType: TextInputType.emailAddress,
-                  isPassword: true),
-                const SizedBox( height:30 ),
-          
+                ),
+                const SizedBox(height: 30),
+                const CustomInputField(
+                    labelText: 'Contraseña',
+                    hintText: 'Contraseña',
+                    keyboardType: TextInputType.emailAddress,
+                    isPassword: true),
+                const SizedBox(height: 30),
                 ElevatedButton(
                   child: const SizedBox(
-                    width: double.infinity,
-                    child: Center(child: Text('Guardar'))
-                  ),
+                      width: double.infinity,
+                      child: Center(child: Text('Guardar'))),
                   onPressed: () {
-          
-                  }, 
+                    if (!myFormKey.currentState!.validate()) {
+                      print('formulario no valido');
+                      return;
+                    }
+                  },
                 ),
               ],
             ),
@@ -55,5 +71,3 @@ class InputScreen extends StatelessWidget {
     );
   }
 }
-
-
