a8d74eba64e67359f17bd0080adb3b67b35b533c
Ivan Gonzalez <ivan@iconos.mx>
2023-03-02 17:28:06 -0800

    quitando es posible espacio top y bottom

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -6,18 +6,23 @@ class ListViewBuilderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          // https://picsum.photos/
-          return FadeInImage(
-            width: double.infinity,
-            height: 300,
-            fit: BoxFit.cover,
-            placeholder: AssetImage('assets/jar-loading.gif'),
-            image: NetworkImage('https://picsum.photos/500/300?image=${index+1}'),
-          );
-        },
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            // https://picsum.photos/
+            return FadeInImage(
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              image: NetworkImage('https://picsum.photos/500/300?image=${index+1}'),
+            );
+          },
+        ),
       )
     );
   }
