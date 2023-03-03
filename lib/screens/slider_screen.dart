622f41659910da980fd35329d4c0faeccd47838c
Ivan Gonzalez <ivan@iconos.mx>
2023-03-02 16:47:19 -0800

    Solo dejando una imagen

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -14,34 +14,34 @@ class _SliderScreenState extends State<SliderScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Slider')),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Slider(
-            Slider.adaptive(
-              min: 50,
-              value: _sliderValue,
-              max: 400,
-              // divisions: 10,
-              onChanged: (value) {
-                _sliderValue = value;
-                setState(() {});
-              }
-            ),
-            Image(
-              image: const NetworkImage('https://static.wikia.nocookie.net/thelastofus/images/b/b1/Stalker_models.png/revision/latest?cb=20200711175035'),
-              fit: BoxFit.contain,
-              width: _sliderValue,
-            ),
-            const SizedBox(height: 100),
-            Image(
-              image: const NetworkImage('https://static.wikia.nocookie.net/thelastofus/images/7/72/Runner_models.png/revision/latest?cb=20200711175107'),
-              fit: BoxFit.contain,
-              width: _sliderValue,
-            ),
-            const SizedBox(height: 100),
-          ],
-        ),
+      body: Column(
+        children: [
+          // Slider(
+          Slider.adaptive(
+            min: 50,
+            value: _sliderValue,
+            max: 400,
+            // divisions: 10,
+            onChanged: (value) {
+              _sliderValue = value;
+              setState(() {});
+            }
+          ),
+          Image(
+            image: const NetworkImage('https://static.wikia.nocookie.net/thelastofus/images/b/b1/Stalker_models.png/revision/latest?cb=20200711175035'),
+            fit: BoxFit.contain,
+            width: _sliderValue,
+          ),
+          const SizedBox(height: 100),
+          // De ser necesario se envielve en un
+          // Exapnded / SingleChildScrollView
+          // Image(
+          //   image: const NetworkImage('https://static.wikia.nocookie.net/thelastofus/images/7/72/Runner_models.png/revision/latest?cb=20200711175107'),
+          //   fit: BoxFit.contain,
+          //   width: _sliderValue,
+          // ),
+          // const SizedBox(height: 100),
+        ],
       ),
     );
   }
