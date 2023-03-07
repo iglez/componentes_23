7577e7b4ff4b1e080eb958e2f744909632e761e0
Ivan Gonzalez <ivan@iconos.mx>
2023-03-06 16:14:22 -0800

    Utilizando un list de ids

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -8,28 +8,30 @@ class ListViewBuilderScreen extends StatefulWidget {
 }
 
 class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+  final List<int> imagesIDs = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: MediaQuery.removePadding(
-        context: context,
-        removeTop: true,
-        removeBottom: true,
-        child: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            // https://picsum.photos/
-            return FadeInImage(
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
-              placeholder: AssetImage('assets/jar-loading.gif'),
-              image: NetworkImage('https://picsum.photos/500/300?image=${index+1}'),
-            );
-          },
-        ),
-      )
-    );
+        backgroundColor: Colors.black,
+        body: MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          removeBottom: true,
+          child: ListView.builder(
+            itemCount: imagesIDs.length,
+            itemBuilder: (context, index) {
+              // https://picsum.photos/
+              return FadeInImage(
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+                placeholder: AssetImage('assets/jar-loading.gif'),
+                image: NetworkImage(
+                    'https://picsum.photos/500/300?image=${ imagesIDs[index] }'),
+              );
+            },
+          ),
+        ));
   }
 }
